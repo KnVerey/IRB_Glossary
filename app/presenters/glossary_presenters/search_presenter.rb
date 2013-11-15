370d321820d7ba0383ab13f06bd3dabffe19db60
Katrina Verey <kn.verey@gmail.com>
2013-11-15 14:17:05 -0500

    First commit with accent-neutral sorting using locales instead of manual ruby sort

--- app/presenters/glossary_presenters/search_presenter.rb
+++ app/presenters/glossary_presenters/search_presenter.rb
@@ -7,22 +7,9 @@ module GlossaryPresenters
 		def initialize(glossary, l1, l2, query)
 			@data = Hash.new
 
-			glossary.term_records.where("#{l1} LIKE ?", "%#{query}%").select("#{l1} as source, #{l2} as translation").each do |record|
+			glossary.term_records.where("#{l1} LIKE ?", "%#{query}%").select("#{l1} as source, #{l2} as translation").order("unaccent(#{l1}) ASC").each do |record|
  					data[record.source] = record.translation
 			end
-
-			sort_data!
 		end
-
-		def sort_data!
-			sorted_record_keys = @data.keys.sort_by { |source_term| remove_accents(source_term).downcase }
-			new_record_hash = {}
-
-			sorted_record_keys.each do |source_term|
-				new_record_hash = new_record_hash.merge( { source_term => @data[source_term] } )
-			end
-			@data = new_record_hash
-		end
-
 	end
 end
--- app/presenters/glossary_presenters/show_presenter.rb
+++ app/presenters/glossary_presenters/show_presenter.rb
@@ -8,7 +8,7 @@ module GlossaryPresenters
 		def initialize(glossary, l1, l2)
 			@data = Hash.new({})
 
-			glossary.term_records.select("#{l1} as source, #{l2} as translation").each do |record|
+			glossary.term_records.select("#{l1} as source, #{l2} as translation").order("unaccent(#{l1}) ASC").each do |record|
  				first_letter = remove_accents(record.source[0]).upcase
 
 				if %w[1 2 3 4 5 6 7 8 9 0].include?(first_letter)
@@ -17,25 +17,8 @@ module GlossaryPresenters
  					@data[first_letter] = @data[first_letter].merge( {record.source => record.translation} )
  				end
 			end
-
-			sort_data!
-		end
-
-		def sort_data!
-
-			@data.each do |letter, record_hash|
-				sorted_record_keys = []
-				new_record_hash = {}
-
-				sorted_record_keys = record_hash.keys.sort_by { |key| remove_accents(key).downcase }
-				sorted_record_keys.each do |source_term|
-					new_record_hash = new_record_hash.merge( { source_term => record_hash[source_term] } )
-				end
-				@data[letter] = new_record_hash
-			end
 			@data = Hash[@data.sort]
 		end
-
 	end
 
 end
